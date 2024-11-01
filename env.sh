acac1ab62e60e7b7c5aed52b867a6dbc33dff4f2
Fernando Petros <fernandopetros@gmail.com>
2024-11-01 22:55:11 +0000

    first commit

---
+++ env.sh
@@ -0,0 +1,8 @@
+export KWIN_TRIPLE_BUFFER=0
+export MOZ_ENABLE_WAYLAND=1
+export ELECTRON_OZONE_PLATFORM_HINT=auto
+export GTK_USE_PORTAL=1
+export DISABLE_LAYER_AMD_SWITCHABLE_GRAPHICS_1=1
+export NODEVICE_SELECT=1
+export ICD_DIR="/usr/share/vulkan/icd.d"
+export VK_DRIVER_FILES="${ICD_DIR}/radeon_icd.i686.json:${ICD_DIR}/radeon_icd.x86_64.json"
